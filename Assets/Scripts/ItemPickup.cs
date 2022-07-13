bce7ae6f7289a88c1ea01a730bd1653faf03de14
FatCat1978 <Pacmandevil1@gmail.com>
2022-07-13 20:48:08 -0300

    updates score pickups to follow the player upon getting in range.

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,23 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class ItemPickup : MonoBehaviour
 {
-    public int ScoreOnPickup;
-    public GameObject spawnOnPickup;
-	private bool hasBeenPicked = false;
+	[SerializeField] private int ScoreOnPickup; //how much we add to the score on pickup - this is shown when the object is destroyed.
+	[SerializeField] private GameObject spawnOnPickup; //what particle effect, if any, is shown on the "pickup" phase. can techincally be any object!
 
-	public AudioClip pickupSound;
+	private bool hasBeenPicked = false; //have we been picked up? if we have, we can shrink and go after
+	private GameObject followTarget; //our followtarget - this is almost always going to be the player. probably!
+
+	[SerializeField] private float followSpeed = 12; //per second.
+
+	[SerializeField] private float shrinkspeed = 2; //per second.
+
+	public AudioClip pickupSound; //todo, debug this
+
+	private Vector3 shrinkVector; //how much it shrinks per second. 
+
+	//manages the killing of the object
+	private bool doDestroy = false;
+	private float secondsUntilDestroy = 3; 
 
 	private void Start()
 	{
@@ -18,6 +31,9 @@ public class ItemPickup : MonoBehaviour
 		{
 			audioSource.clip = pickupSound;
 		}
+
+		shrinkVector = new Vector3(shrinkspeed, shrinkspeed, shrinkspeed);
+
 	}
 	private void OnTriggerEnter(Collider other)
 	{
@@ -26,17 +42,64 @@ public class ItemPickup : MonoBehaviour
 		if (PlayersInfo != null && !hasBeenPicked)
 		{
 			hasBeenPicked = true;
-			GetComponent<AudioSource>().Play();	
+			followTarget = potentialPlayer;
+			
 			PlayersInfo.currentScore += ScoreOnPickup;
-			preDestroy();
+			onItemPickup();
 		}
 
 
 	}
 
-	public void preDestroy()
+	public void Update()
 	{
-		Destroy(this.gameObject);
+		if(doDestroy)
+        {
+			secondsUntilDestroy -= Time.deltaTime;
+			if (secondsUntilDestroy < 0)
+				Destroy(this.gameObject);
+        }
+		if(hasBeenPicked)
+		{
+			if (!followTarget)
+				preDestroy();
+			else
+			{
+				//move towards the follow target, and shrink down
+				this.transform.localScale -= shrinkVector*Time.deltaTime;
+				if (this.transform.localScale.x < 0)
+				{
+					this.transform.localScale = new Vector3(0, 0, 0);
+				}
+
+				float step = followSpeed * Time.deltaTime;
+				this.transform.position = Vector3.MoveTowards(transform.position, followTarget.transform.position, step);
+
+
+				if(Vector3.Distance(transform.position,followTarget.transform.position) < 0.001f || this.transform.localScale.x < 0.001f)
+				{
+					preDestroy();
+				}
+
+			}
+		}
+	}
+
+	private void preDestroy()
+	{
+		
+		QueueForDestroy(); //send it off to the grave
+	}
+
+	private void onItemPickup()
+	{
+		GetComponent<AudioSource>().Play(); //spawn particles, show score gain, etc.
+		preDestroy();
+	}
+
+	public void QueueForDestroy()
+	{
+		doDestroy = true;
 	}
 
 
